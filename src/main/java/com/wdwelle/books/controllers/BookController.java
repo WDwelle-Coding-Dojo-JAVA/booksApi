067a8822f074d2e4d5617dc50848e185ce60f5bc
WDwelle <willdwelle@frontier.com>
2021-12-07 13:32:35 -0800

    showm page

--- src/main/java/com/wdwelle/books/controllers/BookController.java
+++ src/main/java/com/wdwelle/books/controllers/BookController.java
@@ -1,12 +1,22 @@
 package com.wdwelle.books.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wdwelle.books.models.Book;
+import com.wdwelle.books.services.BookService;
 
 @Controller
 public class BookController {
+	@Autowired
+	BookService bookService;
 	@RequestMapping("/books/{id}")
-	public String index() {
-		return "index.jsp";
+	public String show(@PathVariable("id")Long id,Model model) {
+		Book book = bookService.findBook(id);
+		model.addAttribute("book", book);
+		return "show.jsp";
 	}
 }
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -1,12 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="<a href="http://java.sun.com/jsp/jstl/core">http://java.sun.com/jsp/jstl/core</a>"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>books</title>
 </head>
 <body>
-
+	
 </body>
 </html>
